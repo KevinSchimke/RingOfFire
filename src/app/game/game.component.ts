f443d6696deb10df8d27f8f8af0a28bd4ca64687
KevinSchimke <99587213+KevinSchimke@users.noreply.github.com>
2022-08-09 18:08:22 +0100

    game card modell to shuffle game cards#

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
@@ -7,12 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
   takeCardAnimation = false;
+  game!: Game; // Kevin edit from game: Game; to game!: Game;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.newGame();
   }
 
-  takeCard(){
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard() {
     this.takeCardAnimation = true;
   }
 }
---
+++ src/models/game.ts
@@ -0,0 +1,36 @@
+export class Game {
+    public players: string[] = [];
+    public stack: string[] = [];
+    public playedCards: string[] = [];
+    public currentPlayer: number = 0;
+
+    constructor() {
+        for (let index = 1; index < 14; index++) {
+            this.stack.push('spade_' + index);
+            this.stack.push('hearts_' + index);
+            this.stack.push('diamonds_' + index);
+            this.stack.push('clubs_' + index);
+        }
+
+        shuffle(this.stack);
+    }
+}
+
+function shuffle(array: string[]) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
