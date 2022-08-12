c8cf02e98e3f64f7b6b8b9535eb7bc1640ee2e00
KevinSchimke <99587213+KevinSchimke@users.noreply.github.com>
2022-08-12 19:21:05 +0100

    added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,11 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { GameInfoComponent } from './game-info/game-info.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 
 @NgModule({
@@ -37,7 +42,11 @@ import { GameInfoComponent } from './game-info/game-info.component';
     MatFormFieldModule,
     MatInputModule,
     MatCardModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ring-of-fire-19bda',
+    appId: '1:353875149621:web:5ab4e735f3e0cbab4c027c',
+    storageBucket: 'ring-of-fire-19bda.appspot.com',
+    apiKey: 'AIzaSyBphm3E20SEfexEGro4lej92crUNa1X2hg',
+    authDomain: 'ring-of-fire-19bda.firebaseapp.com',
+    messagingSenderId: '353875149621',
+  },
   production: false
 };
 
